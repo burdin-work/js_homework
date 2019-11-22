c2230082c6c94d73ddc922c66acbe090fd638784
Aleksandr Burdin <burdin.work@gmail.com>
2019-11-22 15:53:18 +0200

    added task 2.3

--- 22.11.2019/homework_2.js
+++ 22.11.2019/homework_2.js
@@ -2,14 +2,14 @@
 // Функция, которая проверяет, является ли строка палиндромом и возвращает соответственно true или false
 
 function isPalindrom(userInput) {
-    let result = ( userInput.split('').reverse().join('') === userInput ) ? true : false;
+    let result = (userInput.split('').reverse().join('') === userInput) ? true : false;
 
     return result
 }
 
 // Выполнение функции
-// let userInput = prompt('Введите строку для проверки: является ли строка палиндромом',0);
-// console.log(`\nTask 2.1\n---------\n Ответ: ${isPalindrom(userInput)}`);
+let userInput = prompt('Введите строку для проверки: является ли строка палиндромом', 0);
+console.log(`\nTask 2.1\n---------\n Ответ: ${isPalindrom(userInput)}`);
 
 
 
@@ -20,14 +20,35 @@ function showUnique(userString) {
     let list = userString.split('');
     let newList = [];
 
-    for(let el of list){
-        if( !newList.includes(el) ){
+    for (let el of list) {
+        if (!newList.includes(el)) {
             newList.push(el);
         }
     }
 
-    return newList.length;
+    return newList;
 }
 
-let userString = prompt('Введите строку для проверки символов на уникальность',0);
-console.log(`\nTask 2.2\n---------\n Количество уникальных символов: ${showUnique(userString)}`);
+let userString = prompt('Введите строку для проверки символов на уникальность', 0);
+console.log(`\nTask 2.2\n---------\n Уникальные символы в строке: ${showUnique(userString)}\n Количество уникальных символов: ${showUnique(userString).length}`);
+
+
+
+//>> Task 2.3
+// Функция, которая принимает на вход массив слов, на выход - список тех из них, которые по 5 символов длиной
+
+function showWords(someList) {
+    let newList = [];
+
+    for (el of someList) {
+        if (el.length === 5) {
+            newList.push(el);
+        }
+    }
+
+    return newList;
+}
+
+// Выполнение функции
+let someList = ['пк', 'картина', 'дерево', 'четыре', 'шесть', 'нора', 'хрест'];
+console.log(`\nTask 2.3\n---------\n Старый список: ${someList}\n Новый список : ${showWords(someList)}`);
