8ad114332b1064779cf07bf30d377f6e0991c1d4
Aleksandr Burdin <burdin.work@gmail.com>
2019-11-22 15:23:56 +0200

    added task 2.2

--- 22.11.2019/homework_2.js
+++ 22.11.2019/homework_2.js
@@ -2,11 +2,32 @@
 // Функция, которая проверяет, является ли строка палиндромом и возвращает соответственно true или false
 
 function isPalindrom(userInput) {
-    let result = userInput.split('').reverse().join('') === userInput ? true : false;
+    let result = ( userInput.split('').reverse().join('') === userInput ) ? true : false;
 
     return result
 }
 
 // Выполнение функции
-let userInput = prompt('Введите строку для проверки: является ли строка палиндромом',0);
-console.log(`\nTask 2.1\n---------\n Ответ: ${isPalindrom(userInput)}`);
+// let userInput = prompt('Введите строку для проверки: является ли строка палиндромом',0);
+// console.log(`\nTask 2.1\n---------\n Ответ: ${isPalindrom(userInput)}`);
+
+
+
+//>> Task 2.2
+// Функция, которая принимает строку и выдает на экран к-во уникальных символов в ней (регистр  не важен)
+
+function showUnique(userString) {
+    let list = userString.split('');
+    let newList = [];
+
+    for(let el of list){
+        if( !newList.includes(el) ){
+            newList.push(el);
+        }
+    }
+
+    return newList.length;
+}
+
+let userString = prompt('Введите строку для проверки символов на уникальность',0);
+console.log(`\nTask 2.2\n---------\n Количество уникальных символов: ${showUnique(userString)}`);
