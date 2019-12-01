d87eb6b8b4c5afa40ea3313cb2c5c24354ad964a
Aleksandr Burdin <burdin.work@gmail.com>
2019-12-01 17:33:38 +0200

    + edits

--- 01.12.2019/homework_4.js
+++ 01.12.2019/homework_4.js
@@ -2,6 +2,22 @@
 // Отображение таблицы, в которой выводятся поля поля: name, region, popupation, area
 // со звездочкой: картинка флага по ресурсу из поля flag, первую колонку с порядковым номером, колонку валют (поле currencies), валюты через запятую
 
+function getCurrencies(obj) {
+
+    let  currencList = [];
+
+    for (let i in obj) {
+
+        if (obj[i].name == null) continue;
+
+        currencList.push(obj[i].name);
+
+    }
+
+    return currencList.join(', ');
+}
+
+
 $.ajax({
 
     url: 'https://restcountries.eu/rest/v2/all',
@@ -11,25 +27,10 @@ $.ajax({
         let str = '';
         let startNum = 1;
 
+
         for (let el of data) {
 
-
-            function getCurrencies() {
-
-                let  currencList = [];
-
-                for (let i in el.currencies) {
-
-                    if (el.currencies[i].name == null) continue;
-
-                    currencList.push(el.currencies[i].name);
-                }
-
-                return currencList.join(', ');
-            }
-
-
-            str += '<tr><td>' + ( startNum++ ) + '. </td><td>' + el.name + '</td><td>' + el.region + '</td><td>' + el.population + '</td><td>' + el.area + '</td><td><img src="' + el.flag + '"></td><td>' + getCurrencies() + '</td></tr>'
+            str += '<tr><td>' + ( startNum++ ) + '. </td><td>' + el.name + '</td><td>' + el.region + '</td><td>' + el.population + '</td><td>' + el.area + '</td><td><img src="' + el.flag + '"></td><td>' + getCurrencies(el.currencies) + '</td></tr>'
         }
 
         document.getElementById("someTable").innerHTML = str;
